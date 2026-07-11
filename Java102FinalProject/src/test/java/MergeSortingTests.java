79eec76c7c8595a326b09d4a2dfa4ecdc1d1564a
Max <maxchumak24@gmail.com>
2026-07-11 21:35:39 +0300

    tests

--- Java102FinalProject/src/test/java/MergeSortingTests.java
+++ Java102FinalProject/src/test/java/MergeSortingTests.java
@@ -34,9 +34,9 @@ public class MergeSortingTests {
         users.add(new User.Builder().addName("Yana").addEmail("jana@gmail.com").build());
 
         MergeSort.sort(users, new EmailComparator());
-        assertEquals("brs@gmail.com", users.get(0).getName());
-        assertEquals("eeeegor@gmail.com", users.get(1).getName());
-        assertEquals("vlad@gmail.com", users.get(4).getName());
+        assertEquals("brs@gmail.com", users.get(0).getEmail());
+        assertEquals("eeeegor@gmail.com", users.get(1).getEmail());
+        assertEquals("vlad@gmail.com", users.get(4).getEmail());
     }
     @Test
     void testShouldHandleEmptyList() {
@@ -55,8 +55,8 @@ public class MergeSortingTests {
         users.add(new User.Builder().addName("Alisa").addEmail("liza@gmail.com").build());
 
         MergeSort.sort(users, new UserNameComparator());
-        assertEquals("Alisa", users.get(0).getEmail());
-        assertEquals("Yana", users.get(4).getEmail());
+        assertEquals("Alisa", users.get(0).getName());
+        assertEquals("Yana", users.get(5).getName());
     }
 
 }
